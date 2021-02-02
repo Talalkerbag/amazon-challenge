ce739063e47dfe7c6f840dca546271097fc39775
Talal <kerbagtalal94@hotmail.com>
2021-02-02 13:42:31 -0500

    set up header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDuRWJ50uLIg2-aLSjmC1DTIja9Jta-t5o",
+    authDomain: "challenge-474b9.firebaseapp.com",
+    projectId: "challenge-474b9",
+    storageBucket: "challenge-474b9.appspot.com",
+    messagingSenderId: "522551263288",
+    appId: "1:522551263288:web:a19304910b406bd26e66cd",
+    measurementId: "G-70GPBS8XS6"
+  };
